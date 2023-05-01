98386a4ec023b69af557e74d75864f98f63bbaa5
StowasserH <git@stowasser.tv>
2023-05-01 15:12:50 +0200

    documentation

--- README.md
+++ README.md
@@ -3,10 +3,12 @@ A small script that automates manual fishing in World of Warcraft.
 
 # Directions:
  
-  * stand by a body of water. Make sure that the water is not too bright, otherwise the contrast with the float may not be sufficient.
+  * Stand by a body of water. Make sure that the water is not too bright, otherwise the contrast with the float may not be sufficient.
   * Scroll completely into the character so that the 1st. person view is active. Thus, the float is not covered by your body.
+  * Make sure you have autolooting turned on.
   * Equip a fishing rod.
   * Make sure that no monsters swim by in the water, this would make float detection difficult.
+  * You may have to adjust some parameters in the script manually. Such as the screen resolution and the position of the fishing icon.
   * Start the script. 
   * Attention, you should not leave the device. Unobserved fishing could lead to a ban from the server or the deletion of the account!
 
@@ -22,6 +24,7 @@ You need some software to run this script
   * PIL
 
 #### Recommend
+
   * I use Autokey to start and stop the script. The hotkey function is very useful.
   * The script can use two methods to perform mouse clicks in the wow clinet. Unfortunately, pyautogui does not work 100 percent reliably when catching the fish. That's why I added the possibility to use xte. This made it possible to achieve westward better stability.
 
--- fisher.py
+++ fisher.py
@@ -6,7 +6,6 @@
 
 __author__ = "harald@stowasser.tv"
 __copyright__ = "2023 Harald Stowasser"
-__credits__ = ["12345ieee"]
 __license__ = "MIT"
 __maintainer__ = "Harald Stowasser"
 __email__ = "harald@stowasser.tv"
@@ -26,7 +25,7 @@ res_y = 1080
 # Threshold splash
 th_sp = 25
 # rgb values for the swimmer
-sw = (111, 95, 48)
+sw = (120, 100, 50)
 # Threshold swimmer detection
 th_sw = 4
 # Verbose
@@ -44,17 +43,28 @@ y_max = res_y - res_y // 2 + 100
 end_script = False
 
 
-def xte(command):
+def xte(command: str) -> str:
+    """
+    Sends a xte commando from the system level.
+    
+    :param command: the commando
+    :return:
+    """
     # print(command)
-    dummy_string = "" + system.exec_command("xte " + command + " && echo 'done'", True)
+    # A somewhat strange construct, but the system command requires this dummy.
+    dummy_string: str = "" + system.exec_command("xte " + command + " && echo 'done'", True)
     return dummy_string
 
 
-if v > 0:
-    print(" search from " + str(x_min) + ":" + str(y_min) + " to " + str(x_max) + ":" + str(y_max))
+def interpolate_area(x: int, y: int, px: object) -> tuple:
+    """
+    Calculates the average values of a pixel area.
 
-
-def check_float(x, y, px):
+    :param x: x position of the area
+    :param y: y position of the area
+    :param px: The container for the pixel values
+    :return: tuple of r g b vaues
+    """
     n = r = g = b = 0
     for xx in range(x - 10, x + 10, 2):
         for yy in range(y - 10, y + 10, 2):
@@ -67,44 +77,59 @@ def check_float(x, y, px):
     # rgb(111, 95, 68)
 
 
-def find_float():
+def find_float() -> object:
+    """
+    Look for the fishing float
+
+    :return: None or a tuple of int [brightnes,x pos,y pos,r,g,b]
+    """
     finds = []
     x, y = pyautogui.position()
     if v > 1:
         print("  find from " + str(x) + " " + str(y))
-    px = ImageGrab.grab().load()
+    px: None = ImageGrab.grab().load()
+    # create an aray of posible positions
     for xx in range(x_min, x_max, 4):
         for yy in range(y_min, y_max, 4):
             if px[xx, yy][0] > sw[0] and px[xx, yy][1] > sw[1] and px[xx, yy][2] > sw[1]:
-                r, g, b = check_float(xx, yy, px)
+                r, g, b = interpolate_area(xx, yy, px)
                 finds.append((r + g + b, xx, yy, r, g, b))
+    # if too many positions are found, the image will be too bright and no good results can be expected
     if len(finds) > 40:
         if v > 0:
             print("  to much findings:" + str(len(finds)))
         return None
+    # Sort the areas by brightness
     s_finds = sorted(finds, key=lambda x: x[0])
     found = False
+    # position the mouse on the found position and check whether the float has become brighter.
     while s_finds:
         last = s_finds.pop()
         pyautogui.moveTo(last[1], last[2])
         time.sleep(0.1)
         px2 = ImageGrab.grab().load()
-        r, g, b = check_float(last[1], last[2], px2)
+        r, g, b = interpolate_area(last[1], last[2], px2)
         if v > 1:
             print("  ." + str(last[1]) + " " + str(last[2]))
         if r > last[3] + th_sw and g > last[3] + th_sw and b > last[4] + th_sw:
             return last
-    time.sleep(1)
+    #time.sleep(0.1)
     return None
 
 
-def wait_splash(last):
+def wait_splash(last: tuple) -> bool:
+    """
+    Just wait until the splash lights up the area
+
+    :param last: tuple of int [brightnes,x pos,y pos,r,g,b]
+    :return:
+    """
     if v > 0:
         print("found " + str(last))
     start_time = time.time()
     while time.time() - start_time < 12:
         px2 = ImageGrab.grab().load()
-        r, g, b = check_float(last[1], last[2], px2)
+        r, g, b = interpolate_area(last[1], last[2], px2)
         if r > last[3] + th_sp and g > last[3] + th_sp and b > last[4] + th_sp:
             return True
         time.sleep(0.1)
@@ -112,6 +137,9 @@ def wait_splash(last):
 
 
 def fish():
+    """
+    Click on the fishing icon
+    """
     time.sleep(0.5)
     pyautogui.click(x=b_fish[0], y=b_fish[1], button='left')
     time.sleep(0.5)
@@ -133,6 +161,9 @@ def check_end():
         return True
 
 
+if v > 0:
+    print(" search from " + str(x_min) + ":" + str(y_min) + " to " + str(x_max) + ":" + str(y_max))
+# The main function for auto fishing.
 while check_end():
     fish()
     time.sleep(0.5)
